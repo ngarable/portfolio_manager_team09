d282b1c519103f4c947aca21858eead25ef0ad7e
Blessing Ngara <ngarable@iastate.edu>
2025-07-31 13:23:22 -0500

    Added yFinance services

--- backend/app/services/yfinanceService.py
+++ backend/app/services/yfinanceService.py
@@ -1,8 +1,44 @@
-# TODO: yfinanceService.py service:
-# This service is responsible for fetching financial data from Yahoo Finance.
-#  two main methods: 
-    #  getMarketPrice(ticker) - Fetches the current market price of a given ticker.
+# # services/yfinanceService.py
 
-    # getAssetType(ticker) - Fetches the asset type (e.g., stock, bond) for a given ticker.
+import requests_cache
+requests_cache.install_cache('yfinance_cache', expire_after=86400)  # Cache for 1 day
 
-# TODO: BLESSING
+import yfinance as yf
+
+# Fetch current market price
+def getMarketPrice(ticker):
+    try:
+        stock = yf.Ticker(ticker)
+        current_price = stock.info.get("regularMarketPrice")
+        return current_price
+    except Exception as e:
+        print(f"Error fetching market price for {ticker}: {e}")
+        return None
+
+# Fetch asset type (e.g. stock, etf, mutualfund)
+def getAssetType(ticker):
+    try:
+        stock = yf.Ticker(ticker)
+        asset_type = stock.info.get("quoteType")
+        return asset_type
+    except Exception as e:
+        print(f"Error fetching asset type for {ticker}: {e}")
+        return None
+    
+
+#Testing Code
+# if __name__ == "__main__":
+#     print("Testing yfinanceService...\n")
+
+#     ticker = "AAPL"
+#     ticker2 = "AMD"
+
+#     price = getMarketPrice(ticker)
+#     print(f"Market Price of {ticker}: {price}")
+
+#     price2 = getMarketPrice(ticker2)
+#     print(f"Market Price of {ticker2}: {price2}")
+
+#     asset_type = getAssetType(ticker)
+#     print(f"Asset Type of {ticker}: {asset_type}")
+
